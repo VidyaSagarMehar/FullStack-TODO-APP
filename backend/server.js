a319c9300e067760ad15dc172d66562df8507683
VidyaSagarMehar <vidyasagark890@gmail.com>
2022-11-29 12:17:26 +0530

    changed the url from https to http

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-	console.log(`App is running at the https://localhost:${PORT}`);
+	console.log(`App is running at the http://localhost:${PORT}`);
 });

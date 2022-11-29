93ebcd134503a4de93d51fd65211938bbcdca4e9
VidyaSagarMehar <vidyasagark890@gmail.com>
2022-11-29 12:12:01 +0530

    schema for user athuentication added

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectToDb = () => {
 	mongoose
-		.connect(process.env.MONGO_URI)
+		.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
 		.then((conn) => {
 			console.log(`Connected to DB : ${conn.connection.host}`);
 		})

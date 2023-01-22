b16f4c645613b467ddb9cd4a398dbc0b40cd19d9
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-01-22 21:39:31 +0530

    cors added in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const connectToDb = require('./config/db');
 const notesRoutes = require('./routes/notesRoute');
 const userRoutes = require('./routes/userRoute');
+const cors = require('cors');
+
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 connectToDb();
